53ac51be2404d7604a9d353ab7f16cfb508a991e
Jmint <jeffreymintah737@gmail.com>
2024-05-09 11:48:27 +0000

    changes

--- socialapp/utils.py
+++ socialapp/utils.py
@@ -12,8 +12,7 @@ class Auth():
         try:
             id_info = id_token.verify_oauth2_token(access_token, requests.Request())
             if "accounts.google.com" in id_info['iss']:
-                 
-            
+                return id_info
         except Exception as e:
             return 'Token has expired or is Invlaid'
             
