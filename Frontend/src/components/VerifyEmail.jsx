eb41c8e1fc41310a0c97274c5b156f8abd21e115
Jmint <jeffreymintah737@gmail.com>
2024-05-09 13:44:53 +0000

    changes

--- Frontend/src/components/VerifyEmail.jsx
+++ Frontend/src/components/VerifyEmail.jsx
@@ -1,7 +1,6 @@
 
 import React, {useState} from 'react'
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+
 
 const VerifyEmail = () => {
     const [otp, setOtp]=useState("")
