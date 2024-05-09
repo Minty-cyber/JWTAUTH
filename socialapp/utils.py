d80db7f19529c8611e7a6c174577b4d7c609e8f8
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:07:17 +0000

    changes

--- socialapp/utils.py
+++ socialapp/utils.py
@@ -17,7 +17,7 @@ class Auth():
             return 'Token has expired or is Invlaid'
 
     def login_user(email, password):
-        login_user = authenticate(email=email, password=password)
+        user = authenticate(email=email, password=password)
         token = user.tokens()
         return {
             'email': user.email,
