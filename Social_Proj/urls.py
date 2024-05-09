fa98cdd46504d4e5b7c2c3ab170e29bc86ee6cb7
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:30:05 +0000

    changes

--- Social_Proj/urls.py
+++ Social_Proj/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('account.urls')),
-    path('api/auth/', include('account.urls')),
+    path('api/auth/', include('social.urls')),
     
     
 ]
