8418c91454a1d69af9b3057d58a14ee720481ec0
Jmint <jeffreymintah737@gmail.com>
2024-05-09 11:50:11 +0000

    changes

--- socialapp/utils.py
+++ socialapp/utils.py
@@ -19,6 +19,6 @@ class Auth():
     def register_social_user(provider, email, first_name, last_name):
         user = User.objects.filter(email=email)
         if user.exists():
-            if provider == user[0]:
+            if provider == user[0].auth_provider
         
             
