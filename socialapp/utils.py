1ea288eba08c9ad67c8f06a8aeeb7337df7164c5
Jmint <jeffreymintah737@gmail.com>
2024-05-09 11:39:44 +0000

    changes

--- socialapp/utils.py
+++ socialapp/utils.py
@@ -3,4 +3,4 @@ from google.oauth2 import id_token
 from account.models import User
 from django.contrib.auth import authenticate
 from django.conf import settings
-from rest_framework
+from rest_framework.exceptions import AuthenticationFailed
