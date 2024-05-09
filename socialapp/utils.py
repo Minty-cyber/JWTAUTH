35dfa7892ed0333e7c54b6538e578c7d220b452c
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:07:11 +0000

    changes

--- socialapp/utils.py
+++ socialapp/utils.py
@@ -20,8 +20,8 @@ class Auth():
         login_user = authenticate(email=email, password=password)
         token = user.tokens()
         return {
-            'email': login_user.email,
-            'full_name': login_user.get_name,
+            'email': user.email,
+            'full_name': user.get_name,
             'access_token': str(token.get('access')),
             'refresh_token': str(token.get('refresh'))
         }
