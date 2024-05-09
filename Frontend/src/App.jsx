e934da75b8aa0b67d4f0240f8e4aba5e65bb8081
Jmint <jeffreymintah737@gmail.com>
2024-05-09 13:01:30 +0000

    changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element = {<SignUp/>} />
+        <Route path='/' element = {<SignUp/>} />
         <Route path='/' element = {<LogIn/>} />
 
       </Routes>
