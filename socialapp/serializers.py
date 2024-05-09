1732ea5a50fb3eec565ff5a91f04fe6841628b31
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:14:47 +0000

    changes

--- socialapp/serializers.py
+++ socialapp/serializers.py
@@ -2,3 +2,6 @@ from rest_framework import serializers
 from .utils import Auth, register_social_user
 from django.conf import settings
 from rest_framework.exceptions import AuthenticationFailed
+
+
+
