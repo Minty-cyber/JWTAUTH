f408d4aea03591b270d2a2c37647b2a7cb0ab445
Jmint <jeffreymintah737@gmail.com>
2024-05-09 13:29:30 +0000

    changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { SignUp, LogIn, VerifyEmail, ForgotPass } from "./components"
+import { SignUp, LogIn, VerifyEmail} from "./components"
 
 function App() {
   return (
