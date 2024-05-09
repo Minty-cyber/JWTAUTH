f0c65fd432acda52ef8a69bea83c82492ee68a1a
Jmint <jeffreymintah737@gmail.com>
2024-05-09 13:27:01 +0000

    changes

---
+++ Frontend/src/components/index.jsx
@@ -0,0 +1,14 @@
+import SignUp from "./SignUp";
+import LogIn from "./LogIn";
+import Profile from "./Profile";
+import ForgotPass from "./ForgotPass";
+import VerifyEmail from "./VerifyEmail";
+
+export {
+    SignUp,
+    LogIn,
+    Profile,
+    ForgotPass,
+    VerifyEmail
+}
+
