7848312c5798d9047d84b1e0ad42813d8a0cd45c
Jmint <jeffreymintah737@gmail.com>
2024-05-09 11:16:22 +0000

    changes

--- account/models.py
+++ account/models.py
@@ -15,7 +15,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
-    auth_provider = models.CharField
+    auth_provider = models.CharField(max_length=50, default=)
     
     USERNAME_FIELD = 'email'
     
