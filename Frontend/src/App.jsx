74ec8ac6a4f3388c0687bd9107f4b8c53d95d769
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:53:32 +0000

    changes

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import { SignUp, LogIn, VerifyEmail, ForgotPass } from "./components"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <Router>
+      <Routes>
+        <Route path='/'
+          element = {<SignUp/>}
+        >
+
+        </Route>
+      </Routes>
+    </Router>
+      
+    </>
+  )
+}
+
+export default App
