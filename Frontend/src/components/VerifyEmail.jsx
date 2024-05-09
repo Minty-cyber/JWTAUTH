bfe7e2a4471f231bab9d876d63ed7a79cec47b61
Jmint <jeffreymintah737@gmail.com>
2024-05-09 13:45:43 +0000

    changes

--- Frontend/src/components/VerifyEmail.jsx
+++ Frontend/src/components/VerifyEmail.jsx
@@ -2,33 +2,18 @@ import React, {useState} from 'react'
 
 
 const VerifyEmail = () => {
-    const [otp, setOtp]=useState("")
-    const navigate=useNavigate()
-
-    const handleOtpSubmit = async(e)=>{
-            e.preventDefault()
-            if (otp) {
-                const res = await axios.post('http://localhost:8000/api/v1/auth/verify-email/', {'otp':otp})
-                const resp = res.data
-                if (res.status === 200) {
-                    navigate('/login')
-                    toast.success(resp.message)
-                }
-                
-            }
-            
-    }
+    
   return (
     <div>
         <div className='form-container'>
-            <form action="" style={{width:"30%"}} onSubmit={handleOtpSubmit}>
+            <form action="" style={{width:"30%"}}>
                <div className='form-group'>
                  <label htmlFor="">Enter your Otp code:</label>
                  <input type="text"
                   className='email-form'  
                   name="otp"
-                  value={otp}
-                  onChange={(e)=>setOtp(e.target.value)} 
+                //   value={otp}
+                //   onChange={(e)=>setOtp(e.target.value)} 
                    />
                </div>
                <button type='submit' className='vbtn'>Send</button>
