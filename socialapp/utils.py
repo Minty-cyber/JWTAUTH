7349a836ce5fba10f4a36ea1dc906c04d6170393
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:08:10 +0000

    changes

--- socialapp/utils.py
+++ socialapp/utils.py
@@ -31,7 +31,7 @@ class Auth():
         user = User.objects.filter(email=email)
         if user.exists():
             if provider == user[0].auth_provider:
-               auth_user = login_user()
+               auth_user = login_user(email, settings.SOCIAL_AUTH_PASSWORD)
             else:
                 raise AuthenticationFailed(
                     detail=f'Please login with {user[0].auth_provider}'
