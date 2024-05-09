e4cb2308613df22130a15bad92f349ac7c2d8dc7
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:20:30 +0000

    changes

--- socialapp/serializers.py
+++ socialapp/serializers.py
@@ -23,4 +23,5 @@ class AuthSerializer(serializers.Serializer):
                 detail=f"Could not verify user"
             )
         email = google_user_data['email']
+        first_name = google_user_data['give_name']
         
