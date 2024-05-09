86b9c50c6d928d62a4acaceb20863a70e6f959e1
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:27:24 +0000

    changes

--- socialapp/views.py
+++ socialapp/views.py
@@ -11,4 +11,5 @@ class AuthSignInView(GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = ((serializer.validated_data)['access_token'])
+        return Response(data, status=status.HTTP_200_OK)
         
