4f0bfdfd28af798d079edc8246ba128de1c4a10c
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:23:38 +0000

    changes

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from .serializers import AuthSerializer
-from rest_framework.response import
+from rest_framework.response import Response
+from rest_framework import
 
 # Create your views here.
