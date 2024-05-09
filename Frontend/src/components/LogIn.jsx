d907733f6183916a09f614ac3c4b51064e46fd32
Jmint <jeffreymintah737@gmail.com>
2024-05-09 13:47:52 +0000

    changes

--- Frontend/src/components/LogIn.jsx
+++ Frontend/src/components/LogIn.jsx
@@ -2,7 +2,56 @@ import React from 'react'
 
 const LogIn = () => {
   return (
-    
+    <div className='form-container'>
+    <div className='wrapper'>
+      <div>
+        <h2>Create Account</h2>
+        <form action="">
+            <div className='form-group'>
+               <label htmlFor="">Email Address:</label>
+               <input type="text"
+                className='email-form'  
+                name="email" 
+                // value={email}  
+                />
+             </div>
+             <div className='form-group'>
+               <label htmlFor="">First Name</label>
+               <input type="text"
+                className='email-form'  
+                name="first_name" 
+                // value={email}  
+                />
+             </div>
+             <div className='form-group'>
+               <label htmlFor="">Last Name</label>
+               <input type="text"
+                className='email-form'  
+                name="last_name" 
+                // value={email}  
+                />
+             </div>
+             <div className='form-group'>
+               <label htmlFor="">Password</label>
+               <input type="password"
+                className='email-form'  
+                name="password" 
+                // value={email}  
+                />
+             </div>
+             <div className='form-group'>
+               <label htmlFor="">Confirm Password:</label>
+               <input type="text" 
+               className='p'  
+               name="password2" 
+              //  value={password2} 
+               />
+             </div>
+             <input type="submit" value= "Submit" className='submitButton' />
+        </form>
+      </div>
+    </div>
+  </div>
   )
 }
 
