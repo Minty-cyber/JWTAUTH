19d7714595a9aa8a6cc2113846e1992d79a4c15a
Jmint <jeffreymintah737@gmail.com>
2024-05-09 12:51:53 +0000

    changes

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -2,5 +2,13 @@ import SignUp from "./SignUp";
 import Login from "./LogIn";
 import Profile from "./Profile";
 import ForgotPass from "./ForgotPass";
-import VerifyEmail from "./"
+import VerifyEmail from "./VerifyEmail";
+
+export {
+    SignUp,
+    Login,
+    Profile,
+    ForgotPass,
+    VerifyEmail
+}
 
