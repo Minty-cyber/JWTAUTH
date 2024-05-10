3dce98f89551af418c5a7575d5a63c3c5e54832a
Jmint <jeffreymintah737@gmail.com>
2024-05-10 13:23:42 +0000

    changes

--- Frontend/src/components/SignUp.jsx
+++ Frontend/src/components/SignUp.jsx
@@ -39,7 +39,7 @@ const SignUp = () => {
       setTimeout(() => {
         setLoading(false); //Removing the loading from there
         setSuccessMessage("Account Created Successfully") //Initializing the message
-      },)
+      },2000)
       
       
     }
