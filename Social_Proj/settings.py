9f5c558759b35309e819558f718eae20f3848761
Jmint <jeffreymintah737@gmail.com>
2024-05-10 14:17:38 +0000

    adding my frontend endpoint to the corsheaders origins

--- Social_Proj/settings.py
+++ Social_Proj/settings.py
@@ -174,6 +174,6 @@ SOCIAL_AUTH_PASSWORD = env('SOCIAL_AUTH_PASSWORD')
 CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
-    "http://localhost:8080",
+    "http://localhost:8000",
     "http://127.0.0.1:9000",
 ]
