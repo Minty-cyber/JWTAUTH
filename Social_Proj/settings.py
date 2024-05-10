a51e4e0c932518cfdebccae68ea3782fd3f4d865
Jmint <jeffreymintah737@gmail.com>
2024-05-10 14:37:32 +0000

    changes

--- Social_Proj/settings.py
+++ Social_Proj/settings.py
@@ -91,10 +91,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Social_Proj.wsgi.application'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-]
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
