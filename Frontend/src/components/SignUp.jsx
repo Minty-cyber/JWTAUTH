13934a8b1a0601ed1cc1ccad56f0d91eca0c6a41
Jmint <jeffreymintah737@gmail.com>
2024-05-10 13:57:59 +0000

    changes

--- Frontend/src/components/SignUp.jsx
+++ Frontend/src/components/SignUp.jsx
@@ -35,7 +35,7 @@ const SignUp = () => {
       }, 2000);
        //the error message disappears after some time
     }else{
-      const res = await axios.post('http://localhost:8000/api/auth/register', formdata)// Add data 
+      const res = await axios.post('http://localhost:8000/api/auth/register', formdata)// Add data to the backend 
 
       if (res.status == 200) {
         
